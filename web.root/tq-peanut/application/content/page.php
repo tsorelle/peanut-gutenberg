f98f275a3c4b43089e1d06003e79c6b1f4be8d66
tsorelle <terry.sorelle@outlook.com>
2026-06-03 09:27:59 -0500

    removed erroneous favicon link

--- web.root/tq-peanut/application/content/page.php
+++ web.root/tq-peanut/application/content/page.php
@@ -40,9 +40,8 @@ if ($embed===1) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php
-        printf('<link rel="icon" type="image/x-icon" href="%s/assets/img/favicon.ico">',URL_APPLICATION)
+      printf('<link rel="icon" type="image/x-icon" href="%s/assets/img/favicon.ico">',URL_APPLICATION)
     ?>
-    <link rel="icon" type="image/x-icon" href="/application/assets/img/favicon.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <?php
     $version = Tops\sys\TConfiguration::getValue('applicationVersionNumber','peanut','version');
