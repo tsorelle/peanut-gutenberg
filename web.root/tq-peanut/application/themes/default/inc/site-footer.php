c0a2eb93c06e333aba70b480ffbc323501687072
tsorelle <terry.sorelle@outlook.com>
2026-06-12 11:35:48 -0500

    added bg-body-tertiary class to footer, to match bootscore theme.

--- web.root/tq-peanut/application/themes/default/inc/site-footer.php
+++ web.root/tq-peanut/application/themes/default/inc/site-footer.php
@@ -2,11 +2,6 @@
     <div class="container">
         <div class="d-flex justify-content-end" style="float: right">
             <?php
-//                /** @var  $editorsignedin */
-//                if ($editorsignedin) {
-                    /** @noinspection HtmlUnknownTarget */
-//                    print '<div class="me-5"><a href="\song\new" class="mr-5">New Song</a></div>';
-//                }
                 /** @var $signin */
                 print $signin;
            ?>
