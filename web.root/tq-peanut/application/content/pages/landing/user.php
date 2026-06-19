414c3949d6afcca1c4124df468516191cdd0ecac
tsorelle <terry.sorelle@outlook.com>
2026-06-19 09:32:14 -0500

    updated landing pages

---
+++ web.root/tq-peanut/application/content/pages/landing/user.php
@@ -0,0 +1,7 @@
+
+<div class="landing-page-image">
+    <?php
+    printf('<img alt="tools" src="%s/assets/img/landing/india-stamp.jpeg" class="img-thumbnail img-fluid">',
+            URL_APPLICATION);
+    ?>
+</div>
